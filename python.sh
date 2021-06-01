751153e1f35a9df420ffda490a7e11d830e9be32
Kyle9021 <54778108+Kyle9021@users.noreply.github.com>
2021-06-01 17:18:21 -0400

    Create python.sh

---
+++ python.sh
@@ -0,0 +1,6 @@
+  GNU nano 4.8                                                                                     python.sh                                                                                               
+#!/bin/bash
+
+source /src/.env
+
+python ./pcs-inspect.py -c $PCEE_CUSTOMER_NAME -u $PCEE_API_URL -a $PCEE_ACCESS_KEY -s $PCEE_SECRET_KEY
